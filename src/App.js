084739e420655903517ef622187c92a2f6dcabab
Mansur Narkulyyev <50501517+mansurnarkulyyev@users.noreply.github.com>
2022-06-26 01:11:52 +0300

    вынос даннык в state

--- src/App.js
+++ src/App.js
@@ -1,17 +1,21 @@
 import React from "react";
 
-import BgVideo from 'components/BgVideo/BgVideo';
+import BgVideo from "components/BgVideo/BgVideo";
 import Sidebar from "components/Sidebar/Sidebar";
-import Main from "components/Main/Main"
+import Main from "components/Main/Main";
 import st from "./App.module.css";
 
 const App = (props) => {
   return (
-      <div className={st.wrap}>
-       <BgVideo/>
-       <Sidebar/>
-       <Main posts={props.posts} dialogsData={props.dialogsData} messagesData={props.messagesData}/>
-      </div>
+    <div className={st.wrap}>
+      <BgVideo />
+      <Sidebar />
+      <Main
+        posts={props.state.futurePage.posts}
+        dialogsData={props.state.dialogsPage.dialogsData}
+        messagesData={props.state.dialogsPage.messagesData}
+      />
+    </div>
   );
 };
 
---
+++ src/Redux/state.js
@@ -0,0 +1,32 @@
+let state = {
+    futurePage:{
+        posts: [
+            { id: "1", message: "nayanayanaya some message", likesCount: 11 },
+            { id: "2", message: "Hahahahahahsr message", likesCount: 25 },
+            { id: "3", message: "Hasomr message", likesCount: 2 },
+            { id: "4", message: "Hahahadjjhahahsr message", likesCount: 44 },
+            { id: "5", message: "abarahsr message", likesCount: 5 },
+            { id: "6", message: "Hdnwjssr message", likesCount: 26 },
+          ],
+    },
+    
+    dialogsPage:{
+        dialogsData: [
+            { id: "1", name: "Frank Sinatra", src: "./images/team/Ellipse 5-1.svg" },
+            { id: "2", name: "Harvey Specter", src: "./images/team/Ellipse 5-2.svg" },
+            { id: "3", name: "Harvey Specter", src: "./images/team/Ellipse 5-3.svg" },
+            { id: "4", name: "Harvey Specter", src: "./images/team/Ellipse 4.svg" },
+            { id: "5", name: "Harvey Specter", src: "./images/team/Ellipse 5.svg" },
+          ],
+          messagesData: [
+            { id: "1", message: "nayanayanaya some message" },
+            { id: "2", message: "wefcf message" },
+            { id: "3", message: "Hahahqxwahwcfacwfwhahsr message" },
+            { id: "4", message: "Hahqxahahaqcwefchahsr message" },
+            { id: "5", message: "Hahahahahahsr message" },
+          ],
+    },
+       
+};
+
+export default state;
